3bf6635114d346ba212e86696b9ba54d97648c50
benja27 <50673336+benja27@users.noreply.github.com>
2023-07-06 16:25:22 -0600

    create cards

--- src/components/Ecard3d.jsx
+++ src/components/Ecard3d.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import VanillaTilt from "vanilla-tilt";
 
-let image = "https://cdn.pixabay.com/photo/2016/10/10/14/13/dog-1728494_1280.png"
+import shoe from "../assets/shoes.png";
 
 function Ecard3d() {
+  const tiltref = useRef([]);
+
+  // useEffect(() => {
+  //   tiltref.current.forEach((ref)=>{
+  //     VanillaTilt.init(ref.current)
+  //   })
+  // }, []);
+  useEffect(() => {
+    VanillaTilt.init(tiltref.current)
+  }, []);
+
   return (
-    <div className='container'>
-      <div className="box">
+    <div className="container">
+      
+      <div className="box" ref={(el) => (tiltref.current[0] = el)}>
         <div className="name">Product Name</div>
-        <a href="#" className='buy'>buy now</a>
+        <a href="#" className="buy">
+          buy now
+        </a>
         <div className="circle"></div>
-        <img className='product' style={{width:"300px"}} src={image} alt="" />
+        <img className="product" src={shoe} alt="" />
       </div>
 
+           
+      <div className="box" ref={(el) => (tiltref.current[1] = el)}>
+        <div className="name">Product Name</div>
+        <a href="#" className="buy">
+          buy now
+        </a>
+        <div className="circle"></div>
+        <img className="product" src={shoe} alt="" />
+      </div>
+      
+      <div className="box" ref={(el) => (tiltref.current[2] = el)}>
+        <div className="name">Product Name</div>
+        <a href="#" className="buy">
+          buy now
+        </a>
+        <div className="circle"></div>
+        <img className="product" src={shoe} alt="" />
+      </div>
+
+
+
+
     </div>
-  )
+  );
 }
 
-export default Ecard3d
+export default Ecard3d;
