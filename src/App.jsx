b8fe946d3c655bc96197ad1a30bd66c211413430
benja27 <50673336+benja27@users.noreply.github.com>
2023-07-07 15:52:47 -0600

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Ecard3d from "./components/Ecard3d";
 import "./App.css";
+import Card from "./components/card";
 
 function App() {
   return (
     <div className="">
-      <Ecard3d></Ecard3d>
+      
     </div>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 function Card() {
   return (
-    <div className="card">
-      <h3 className="text-center">dance?</h3>
-      <div className="card-body text-center">
-        <div className="test-container" >
-          <img src="https://picsum.photos/200" className="test" alt="" />
+    <div className="card-container">
+      <div className="card">
+        <h3 className="text-center">Card</h3>
+        
+        <div className="card-body text-center">
+          <div className="test-container">
+            <img src="https://picsum.photos/200" className="test" alt="" />
+          </div>
         </div>
+        <h3 className="text-center">dance?</h3>
       </div>
-      <h2 className="text-center">Card</h2>
     </div>
   );
 }
